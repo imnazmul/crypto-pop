e5a8e6275e688fb93a4a05c188c2b38a2272505c
imnazmul <nazmulislam2322@gmail.com>
2024-03-06 02:40:49 +0600

    done

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const cryptoRequested = document.getElementById("requested-crypto").value;
+const resultTabContent = document.getElementById("resultTab");
+let cryptoCurrentPrice = '';
 
 document.getElementById("toUSDtext").innerText = `${cryptoRequested}`;
 document.getElementById("crypto-image").innerHTML = `<img src="https://coinicons-api.vercel.app/api/icon/${cryptoRequested.toLowerCase()}"/>`
 
-const logoHandler = async() => {
-    const data = await fetch("https://api3.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-    const r = await data.json();
-    console.log(r)
+
+const crypto = {
+    headers: {
+        'x-access-token': 'coinranking2fa54e52adbbc9d4820c9099de539478bbc2721518cd237d',
+    },
+};
+
+const getCoins = async () => {
+    const response = await fetch('https://api.coinranking.com/v2/coins', crypto);
+    const result = await response.json();
+
+    cryptoCurrentPrice = (Number(result.data.coins[22].price)).toFixed(2); // need mofification about input
+
+    document.getElementById("leftStatics").innerText = `1 LTC = ${cryptoCurrentPrice}`;
+
+    resultTabContent.innerText = `1 LTC = ${cryptoCurrentPrice}`;
 }
 
-logoHandler()
+getCoins();
+
+const calc = document.getElementById("calc");
+
+calc.addEventListener('click', function () {
+    const input = document.getElementById("amountInputed").value;
+    let result = cryptoCurrentPrice * input;
+    Number(result).toFixed(3);
+    resultTabContent.innerText = `${input} LTC = ${result}`;
+})
 
 
 
-// console.log(cryptoImage)
+
+
+
