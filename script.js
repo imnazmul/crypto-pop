c17a3bbd27ad4b4f1350e7e7899bd3c2564ee74a
imnazmul <nazmulislam2322@gmail.com>
2024-03-07 21:56:45 +0600

    api update

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ document.getElementById("toUSDtext").innerText = `${cryptoRequested}`;
 document.getElementById("crypto-image").innerHTML = `<img src="https://coinicons-api.vercel.app/api/icon/${cryptoRequested.toLowerCase()}"/>`
 
 
-const crypto = {
-    headers: {
-        'x-access-token': 'coinranking2fa54e52adbbc9d4820c9099de539478bbc2721518cd237d',
-    },
-};
+
 
 const getCoins = async () => {
-    const response = await fetch('https://api.coinranking.com/v2/coins', crypto);
+    const response = await fetch('https://api.coinbase.com/v2/prices/LTC-USD/buy');
     const result = await response.json();
 
-    cryptoCurrentPrice = (Number(result.data.coins[22].price)).toFixed(2); // need mofification about input
+    cryptoCurrentPrice = (Number(result.data.amount)).toFixed(3); // need mofification about input
 
     document.getElementById("leftStatics").innerText = `1 LTC = ${cryptoCurrentPrice}`;
 
